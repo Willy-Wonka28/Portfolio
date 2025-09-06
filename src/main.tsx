0e761782b5f3783fb8c1e998bbd73be8fbb2b84a
Enenche <adoledavid345@gmail.com>
2025-09-07 00:14:06 +0100

    adding vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.tsx'
 import './App.css'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+<SpeedInsights/>;
